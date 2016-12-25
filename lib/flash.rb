380f110878c2db31f23d658b8595a8d0e45300dd
DanielLChang <daniellawrencechang@gmail.com>
2016-12-25 11:16:56 -0800

    Finish phase 8. Added flash and flash.now methods

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,25 @@
 require 'json'
 
 class Flash
+  attr_reader :now
+
+  def initialize(req)
+    cookie = req.cookies['_rails_lite_app_flash']
+
+    @flash = {}
+    @now = cookie ? JSON.parse(cookie) : {}
+  end
+
+  def [](key)
+    @now[key.to_s] || @flash[key.to_s]
+  end
+
+  def []=(key, val)
+    @flash[key.to_s] = val
+  end
+
+  def store_flash(res)
+    cookie = { path: '/', value: @flash.to_json }
+    res.set_cookie('_rails_lite_app_flash', cookie)
+  end
 end
