13436b64a6d0cc8c7dc7208ccc9d84758374ad56
DanielLChang <daniellawrencechang@gmail.com>
2016-12-24 21:12:31 -0800

    Finish phase 4. Wrote session class added session to controllerbase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -27,6 +27,8 @@ class ControllerBase
     @res.status = 302
     @res['Location'] = url.to_s
 
+    session.store_session(@res)
+
     @already_built_response = true
   end
 
@@ -38,6 +40,8 @@ class ControllerBase
 
     @res['Content-Type'] = content_type
     @res.write(content)
+
+    session.store_session(@res)
 
     @already_built_response = true
   end
@@ -59,6 +63,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
--- lib/session.rb
+++ lib/session.rb
@@ -4,16 +4,27 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    req_cookie = req.cookies['_rails_lite_app']
+
+    if req_cookie
+      @cookie = JSON.parse(req_cookie)
+    else
+      @cookie = {}
+    end
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    cookie_attr = { path: '/', value: @cookie.to_json }
+    res.set_cookie('_rails_lite_app', cookie_attr)
   end
 end
