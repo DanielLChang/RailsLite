99a7516f74016f7745774ae5363ce4cc58bdb628
DanielLChang <daniellawrencechang@gmail.com>
2016-12-26 01:38:58 -0800

    Finished phase 9. Wrote methods for showexception class and rescue template

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,30 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    dir_path = File.dirname(__FILE__)
+    template_file = File.read("#{dir_path}/templates/rescue.html.erb")
+    template = ERB.new(template_file)
+
+    body = template.result(binding)
+
+    ['500', { 'Content-type' => 'text/html' }, body]
   end
 
 end
